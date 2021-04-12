11102e630932046eb1367a2878c496c24d76bb95
emreozkanca <45484445+emreozkanca@users.noreply.github.com>
2021-04-12 16:06:57 +0300

    Series and movie tracking

---
+++ series and movie tracking/Form1.Designer.cs
@@ -0,0 +1,364 @@
+﻿namespace proje
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.circle8 = new proje.Circle();
+            this.circle2 = new proje.Circle();
+            this.circle7 = new proje.Circle();
+            this.circle4 = new proje.Circle();
+            this.circle6 = new proje.Circle();
+            this.circle5 = new proje.Circle();
+            this.circle3 = new proje.Circle();
+            this.circle1 = new proje.Circle();
+            ((System.ComponentModel.ISupportInitialize)(this.circle8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.circle2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.circle7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.circle4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.circle6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.circle5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.circle3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.circle1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(75, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(439, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label1";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(594, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label1";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(2, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(125, 136);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(241, 164);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "label1";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(157)))), ((int)(((byte)(171)))));
+            this.label7.Location = new System.Drawing.Point(90, 378);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "label1";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(125, 311);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 3;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(157)))), ((int)(((byte)(171)))));
+            this.label9.Location = new System.Drawing.Point(439, 378);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 3;
+            this.label9.Text = "label1";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(157)))), ((int)(((byte)(171)))));
+            this.label10.Location = new System.Drawing.Point(250, 378);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(35, 13);
+            this.label10.TabIndex = 3;
+            this.label10.Text = "label1";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(157)))), ((int)(((byte)(171)))));
+            this.label11.Location = new System.Drawing.Point(609, 378);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(35, 13);
+            this.label11.TabIndex = 3;
+            this.label11.Text = "label1";
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Snap ITC", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.ForeColor = System.Drawing.Color.White;
+            this.label12.Location = new System.Drawing.Point(42, 9);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(301, 37);
+            this.label12.TabIndex = 4;
+            this.label12.Text = "Beklenen Diziler";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Snap ITC", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.ForeColor = System.Drawing.Color.White;
+            this.label13.Location = new System.Drawing.Point(42, 211);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(283, 37);
+            this.label13.TabIndex = 4;
+            this.label13.Text = "Eklenen Diziler";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Modern No. 20", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.ForeColor = System.Drawing.Color.Yellow;
+            this.label14.Location = new System.Drawing.Point(677, 13);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(33, 29);
+            this.label14.TabIndex = 5;
+            this.label14.Text = "X";
+            this.label14.Click += new System.EventHandler(this.label14_Click);
+            // 
+            // circle8
+            // 
+            this.circle8.Location = new System.Drawing.Point(572, 265);
+            this.circle8.Name = "circle8";
+            this.circle8.Size = new System.Drawing.Size(101, 89);
+            this.circle8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.circle8.TabIndex = 2;
+            this.circle8.TabStop = false;
+            // 
+            // circle2
+            // 
+            this.circle2.Location = new System.Drawing.Point(572, 60);
+            this.circle2.Name = "circle2";
+            this.circle2.Size = new System.Drawing.Size(101, 89);
+            this.circle2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.circle2.TabIndex = 2;
+            this.circle2.TabStop = false;
+            // 
+            // circle7
+            // 
+            this.circle7.Location = new System.Drawing.Point(234, 265);
+            this.circle7.Name = "circle7";
+            this.circle7.Size = new System.Drawing.Size(101, 89);
+            this.circle7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.circle7.TabIndex = 2;
+            this.circle7.TabStop = false;
+            // 
+            // circle4
+            // 
+            this.circle4.Location = new System.Drawing.Point(234, 60);
+            this.circle4.Name = "circle4";
+            this.circle4.Size = new System.Drawing.Size(101, 89);
+            this.circle4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.circle4.TabIndex = 2;
+            this.circle4.TabStop = false;
+            // 
+            // circle6
+            // 
+            this.circle6.Location = new System.Drawing.Point(69, 265);
+            this.circle6.Name = "circle6";
+            this.circle6.Size = new System.Drawing.Size(101, 89);
+            this.circle6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.circle6.TabIndex = 2;
+            this.circle6.TabStop = false;
+            // 
+            // circle5
+            // 
+            this.circle5.Location = new System.Drawing.Point(410, 265);
+            this.circle5.Name = "circle5";
+            this.circle5.Size = new System.Drawing.Size(101, 89);
+            this.circle5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.circle5.TabIndex = 2;
+            this.circle5.TabStop = false;
+            // 
+            // circle3
+            // 
+            this.circle3.Location = new System.Drawing.Point(69, 60);
+            this.circle3.Name = "circle3";
+            this.circle3.Size = new System.Drawing.Size(101, 89);
+            this.circle3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.circle3.TabIndex = 2;
+            this.circle3.TabStop = false;
+            // 
+            // circle1
+            // 
+            this.circle1.Location = new System.Drawing.Point(410, 60);
+            this.circle1.Name = "circle1";
+            this.circle1.Size = new System.Drawing.Size(101, 89);
+            this.circle1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.circle1.TabIndex = 2;
+            this.circle1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(36)))), ((int)(((byte)(38)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(750, 500);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.circle8);
+            this.Controls.Add(this.circle2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.circle7);
+            this.Controls.Add(this.circle4);
+            this.Controls.Add(this.circle6);
+            this.Controls.Add(this.circle5);
+            this.Controls.Add(this.circle3);
+            this.Controls.Add(this.circle1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximumSize = new System.Drawing.Size(750, 500);
+            this.MinimumSize = new System.Drawing.Size(750, 500);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.circle8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.circle2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.circle7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.circle4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.circle6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.circle5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.circle3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.circle1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Circle circle1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private Circle circle2;
+        private System.Windows.Forms.Label label5;
+        private Circle circle3;
+        private Circle circle4;
+        private System.Windows.Forms.Label label6;
+        private Circle circle5;
+        private Circle circle6;
+        private Circle circle7;
+        private System.Windows.Forms.Label label7;
+        private Circle circle8;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+    }
+}
+
---
+++ series and movie tracking/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Timer t = new Timer();
+       
+        private void t_Tick(object sender, EventArgs e)
+        {
+            int width = this.Width;
+            if (label4.Location.X > width - label4.Width)
+            {
+                circle1.Location = new Point(-200, circle1.Location.Y);
+                label1.Location = new Point(-590, label1.Location.Y);
+
+                circle2.Location = new Point(-50, circle2.Location.Y);
+                label2.Location = new Point(-175, label2.Location.Y);
+
+                circle3.Location = new Point(-600, circle3.Location.Y);
+                label3.Location = new Point(-40, label3.Location.Y);
+
+                circle4.Location = new Point(-400, circle4.Location.Y);
+                label6.Location = new Point(-390, label6.Location.Y);
+
+                circle6.Location = new Point(-600, circle6.Location.Y);
+                label7.Location = new Point(-570, label7.Location.Y);
+
+                circle7.Location = new Point(-400, circle7.Location.Y);
+                label10.Location = new Point(-393, label10.Location.Y);
+
+                circle5.Location = new Point(-200, circle5.Location.Y);
+                label9.Location = new Point(-175, label9.Location.Y);
+
+                circle8.Location = new Point(-50, circle8.Location.Y);
+                label11.Location = new Point(-22, label11.Location.Y);
+
+
+                label4.Location = new Point(-600, label4.Location.Y);
+
+            }
+            else
+            {
+                circle1.Location = new Point(circle1.Location.X + 3, circle1.Location.Y);
+                circle2.Location = new Point(circle2.Location.X + 3, circle2.Location.Y);
+                circle3.Location = new Point(circle3.Location.X + 3, circle3.Location.Y);
+                circle4.Location = new Point(circle4.Location.X + 3, circle4.Location.Y);
+                circle6.Location = new Point(circle6.Location.X + 3, circle6.Location.Y);
+                circle7.Location = new Point(circle7.Location.X + 3, circle7.Location.Y);
+                circle5.Location = new Point(circle5.Location.X + 3, circle5.Location.Y);
+                circle8.Location = new Point(circle8.Location.X + 3, circle8.Location.Y);
+
+
+
+                label1.Location = new Point(label1.Location.X + 3, label1.Location.Y);
+                label2.Location = new Point(label2.Location.X + 3, label2.Location.Y);
+                label3.Location = new Point(label3.Location.X + 3, label3.Location.Y);
+                label4.Location = new Point(label4.Location.X + 3, label4.Location.Y);
+                label6.Location = new Point(label6.Location.X + 3, label6.Location.Y);
+                label7.Location = new Point(label7.Location.X + 3, label7.Location.Y);
+                label10.Location = new Point(label10.Location.X + 3, label10.Location.Y);
+                label9.Location = new Point(label9.Location.X + 3, label9.Location.Y);
+                label11.Location = new Point(label11.Location.X + 3, label11.Location.Y);
+
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            t.Start();
+            t.Interval = 1;
+            t.Tick += new EventHandler(t_Tick);
+            t.Enabled = true;
+
+            HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = web.Load(url:"https://www.dizibox.pw/");
+            
+            foreach (var item in doc.DocumentNode.SelectNodes(xpath: "//ul[@id='recommended_series']"))
+            {
+                label1.Text = item.ChildNodes[1].InnerText.Trim();
+                label6.Text = item.ChildNodes[5].InnerText.Trim();
+                label2.Text = item.ChildNodes[3].InnerText.Trim();
+                label3.Text = item.ChildNodes[7].InnerText.Trim();
+                label7.Text = item.ChildNodes[9].InnerText.Trim();
+                label10.Text = item.ChildNodes[11].InnerText.Trim();
+                label9.Text = item.ChildNodes[13].InnerText.Trim();
+                label11.Text = item.ChildNodes[15].InnerText.Trim();
+
+
+            }
+            circle3.Image = Resim(@"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQc0uDAiKlG59MXaGtloPf1W9vSHCYvFy9wDQ&usqp=CAU");
+            circle4.Image = Resim(@"https://cdn.webrazzi.com/uploads/2018/12/hakan-muhafiz-netflix.jpg");
+            circle1.Image = Resim(@"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ4RwmStk90Xi9emd2mSzai-K-2dKR1Ah3TiA&usqp=CAU");
+            circle2.Image = Resim(@"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRAPoBF7z7Mits_iTKOk3tk4gY3P0_o4eG6Xg&usqp=CAU");
+            circle6.Image = Resim(@"https://imgrosetta.mynet.com.tr/file/12090752/12090752-728xauto.jpg");
+            circle7.Image = Resim(@"https://i4.hurimg.com/i/hurriyet/75/770x0/5912380d18c77326480102c0");
+            circle5.Image = Resim(@"https://i.hizliresim.com/95zNbO.jpg");
+            circle8.Image = Resim(@"https://tr.web.img4.acsta.net/pictures/14/02/16/23/53/181925.jpg?coixp=50&coiyp=39");
+
+           
+
+            Bitmap Resim(string Url)
+            {
+                WebRequest rs = WebRequest.Create(Url);
+                return (Bitmap)Bitmap.FromStream(rs.GetResponse().GetResponseStream());
+            }
+        }
+
+        private void label14_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.dizibox.pw/diziler/stranger-things-izle-2");
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.dizibox.pw/diziler/hakan-muhafiz-the-protector-izle-1");
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.dizibox.pw/diziler/fargo-izle");
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.dizibox.pw/diziler/exatlon-challenge-turkiye-izle/");
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.dizibox.pw/diziler/dark-izle-1");
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.dizibox.pw/diziler/olmek-icin-on-uc-sebep-13-reasons-why-izle");
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.dizibox.pw/diziler/snowpiercer");
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.dizibox.pw/diziler/the-100-izle-1");
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            t.Stop();
+            
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            t.Start();
+        }
+    }
+
+}
